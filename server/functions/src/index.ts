db92364094ddb2aba904422c49177da337aa5787
Francisco Gutierrez <pikilon@gmail.com>
2022-02-17 11:28:31 +0100

    starts api points

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -1,9 +1,15 @@
-import * as functions from "firebase-functions";
+import * as functions from "firebase-functions"
+import fetch from "cross-fetch"
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const getVideosInfo = functions.https.onRequest(
+  async (request, response) => {
+    const twitchUrl = "https://www.twitch.tv/videos/1267815693"
+    const twitchResponse = await fetch(twitchUrl)
+    const twitchData = await twitchResponse.text();
+    functions.logger.info("Hello logs!", { structuredData: true })
+    response.send("Hello from Firebase!" + `<textarea>${twitchData}</textarea>`)
+  }
+)
