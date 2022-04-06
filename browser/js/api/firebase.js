d3d7b8e9fdfade06d96af7f1354ed43c29988e83
Francisco Gutierrez <pikilon@gmail.com>
2022-04-06 10:55:14 +0400

    Injects static files listing using node-js

--- browser/js/api/firebase.js
+++
@@ -1,8 +0,0 @@
-import { initializeApp } from "firebase/app"
-import { getFunctions, httpsCallable  } from "firebase/functions"
-import { FIREBASE_CONFIG } from "../../config.js"
-
-const app = initializeApp(FIREBASE_CONFIG)
-
-const firebaseFunctions = getFunctions(app)
-export const getYoutubeInfo = httpsCallable(firebaseFunctions, "youtube")
