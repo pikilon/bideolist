3cd8841a5460607f5067e4c4e8c5239dbc25ae49
Francisco Gutierrez <pikilon@gmail.com>
2022-04-06 12:52:42 +0400

    Service worker just use module type on registration

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,4 @@
+import ASSETS from "./generated-assets-list-module.js"
 const SERVICE_WORKERS_EVENTS = {
   INSTALL: 'install',
   ACTIVATE: 'activate',
@@ -7,30 +8,17 @@ const SERVICE_WORKERS_EVENTS = {
 const STATIC_CACHE_NAME = 'static-cache-v1';
 
 self.addEventListener(SERVICE_WORKERS_EVENTS.INSTALL, async (event) => {
-  // const clients = await self.clients.matchAll()
-  // console.log('clients.length', clients.length);
-  // for (const client of clients) client.postMessage({ msg: "installed" });
-
+  const cache = caches.open(STATIC_CACHE_NAME);
+  cache.then(cache => {
+    cache.addAll(ASSETS)
+  })
 })
 
 // it doesn't activate it on new version automatically
 self.addEventListener(SERVICE_WORKERS_EVENTS.ACTIVATE, async (event) => {
-  console.log('event', event);
-  if (!event.clientId) return;
-  const client = await clients.get(event.clientId);
-  client.postMessage({ msg: "activated" });
   // console.log('activated', event);
 })
 
 self.addEventListener(SERVICE_WORKERS_EVENTS.FETCH, event => {
   // console.log('fetched', event);
-})
-
-self.addEventListener(SERVICE_WORKERS_EVENTS.MESSAGE, ({ data: { type, payload } }) => {
-  if (type === "ASSET_PATHS") {
-    const cache = caches.open(STATIC_CACHE_NAME);
-    cache.then(cache => {
-      cache.addAll(payload)
-    })
-  }
 })
